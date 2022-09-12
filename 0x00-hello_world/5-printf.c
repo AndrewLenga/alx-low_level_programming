fecda9c31c9bf90231ab4924fc39a879dc408679
AndrewLenga <andrewlenga21@gmail.com>
2022-09-12 00:19:44 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - print the string int put function
+**Description:using the main function
+*this program print "Programming is like building a multilingual parts"
+*Return 0
+*/
+int main(void)
+{
+    printf("with proper grammar, the outcome is a piece of art,\n");
+    return (0);
+}
