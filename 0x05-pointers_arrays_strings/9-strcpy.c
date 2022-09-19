9adee6d82085995abc9db6fb37857c948feac2d1
AndrewLenga <andrewlenga21@gmail.com>
2022-09-19 01:28:06 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,7 +5,7 @@
  * terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
- * Return: A pointer to the destination string @dest.
+ * Return: the pointer to dest.
  */
 char *_strcpy(char *dest, const char *src)
 {
