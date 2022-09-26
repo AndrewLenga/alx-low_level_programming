f306bfd253851bac4b0f7aace9d306813cdb7884
AndrewLenga <andrewlenga21@gmail.com>
2022-09-26 12:13:01 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,9 @@
 void *_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int index;
+
 	unsigned char *destination = dest;
+
 	const unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
